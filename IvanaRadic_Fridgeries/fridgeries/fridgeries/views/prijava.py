96a460dff1377945a76684316b970248f622f705
IvanaRadic <45042077+IvanaRadic@users.noreply.github.com>
2019-06-30 21:55:38 +0200

    Delete prijava.py

--- IvanaRadic_Fridgeries/fridgeries/fridgeries/views/prijava.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-
-bp = Blueprint(__name__, __name__, template_folder='templates')
-
-@bp.route('/prijava')
-def show():
-   return render_template('prijava.html')
